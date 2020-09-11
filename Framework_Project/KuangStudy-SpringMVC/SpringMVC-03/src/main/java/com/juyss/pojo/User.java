ee78ceee538afea38b8fe6ab0a1739da5f5b31e3
juyss <1028507471@qq.com>
2020-09-12 00:05:07 +0800

    add module SpringMVC-03

---
+++ Framework_Project/KuangStudy-SpringMVC/SpringMVC-03/src/main/java/com/juyss/pojo/User.java
@@ -0,0 +1,53 @@
+package com.juyss.pojo;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author ShmeBluk
+ * @version 1.0
+ * @ClassName: User
+ * @Desc:
+ * @package com.juyss.pojo
+ * @project KuangStudy-SpringMVC
+ * @date 2020/9/11 23:32
+ */
+@Component
+public class User {
+
+    private Integer id;
+
+    private String name;
+
+    public User() {
+    }
+
+    public User(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+}
