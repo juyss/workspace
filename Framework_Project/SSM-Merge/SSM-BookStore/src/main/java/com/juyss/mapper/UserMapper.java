1cb3eabd9b8bb2068fbc37ac12817f3042a57b92
juyss <1028507471@qq.com>
2020-09-18 17:39:37 +0800

    add unversioned files

---
+++ Framework_Project/SSM-Merge/SSM-BookStore/src/main/java/com/juyss/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.juyss.mapper;
+
+/**
+ * @author ShmeBluk
+ * @version 1.0
+ * @ClassName: UserMapper
+ * @Desc: 提供与用户相关的数据库操作接口
+ * @package com.juyss.mapper
+ * @project SSM-Merge
+ * @date 2020/9/17 20:04
+ */
+public interface UserMapper {
+
+
+}
